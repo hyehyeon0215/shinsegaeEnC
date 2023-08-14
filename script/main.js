bcfbef5fb3469cb77b62b71e3d5ebd3bba756ac6
hyehyeon0215 <qwert0880@naver.com>
2023-08-15 03:48:36 +0900

    헤더 js 완료 (스크롤/서브)

--- script/main.js
+++ script/main.js
@@ -1,9 +1,25 @@
+const a = document.querySelectorAll('[href="#"]');
+
+a.forEach((el)=>{
+    el.addEventListener("click", (e)=>{
+        e.preventDefault();
+    })
+})
+
 const visual = document.querySelector("#main-visual");
+const frame_idxbox = document.querySelector(".frame-index");
+const frame_index = document.querySelectorAll(".frame-index li");
 const frames = visual.querySelectorAll(".frame li");
-const frame_index = visual.querySelectorAll(".frame-index li");
-const bars = visual.querySelectorAll(".bar");
 const frame_name = visual.querySelectorAll(".frame-name li");
 
+
+const main_header = document.querySelector("#main-header");
+const gnb = main_header.querySelectorAll("nav > .header-gnb >li");
+const sub = main_header.querySelectorAll(".header-sub > li");
+const bg = document.querySelector(".main-bg");
+
+const search_btn = document.querySelector(".search-btn");
+const search_cls = document.querySelector(".search-close");
 
 const interval = 5000;
 const len = frames.length - 1;
@@ -13,32 +29,20 @@ let timer = null;
 
 startRolling();
 
-
 function startRolling() {
-
-    for (let el of bars) el.style.display = "block";
-
-    setTimeout(progress, 0);
     activation(num);
-
-
     timer = setInterval(rolling, interval);
-
 }
-
 
 
 function activation(index) {
     for (let el of frames) el.classList.remove("on");
     for (let el of frame_index) el.classList.remove("on");
     for (let el of frame_name) el.classList.remove("on");
-    for (let el of bars) el.classList.remove("on");
     frames[index].classList.add("on");
     frame_index[index].classList.add("on");
     frame_name[index].classList.add("on");
-    bars[index].classList.add("on");
     num = index;
-    for (let el of bars) el.style.width = "0%";
 }
 
 
@@ -49,35 +53,96 @@ function rolling() {
         num = 0;
     }
     activation(num);
-    progress();
 }
 
 
-function progress() {
-    bars.forEach((el, index)=>{
 
-        if(el.classList = 'on') {
-            const init = 0;
-            const targetValue = 100;
-            const unit = '%';
-            const startTime = performance.now();
-        
-            function animate(time) {
-                const realTime = time - startTime;
-                const prog = realTime / interval;
-                const currentValue = init + ((targetValue - init) * prog);
-    
-                bars[index].style.width = `${currentValue}${unit}`;
-        
-                if(prog < 1) {
-                    requestAnimationFrame(animate);
-                } else {
-                    bars[index].style.width = "0%"
-                    if (typeof callback === "function") callback();
-                }
-            }
-        
-            requestAnimationFrame(animate);
+gnb.forEach((el)=>{
+    el.addEventListener("mouseenter", ()=>{
+        main_header.classList.add('on');
+        bg.classList.add('on');
+    })
+
+    main_header.addEventListener("mouseleave", ()=> {
+        if(main_header.classList.contains('on')) {
+            main_header.classList.remove('on');
+            bg.classList.remove('on');
         }
     })
+
+})
+
+
+search_btn.addEventListener("click", ()=> {
+    if(main_header.classList.contains('search')) {
+        main_header.classList.remove('search');
+        bg.classList.remove('on');
+    }
+    else {
+        main_header.classList.remove('on');
+        main_header.classList.add('search');
+    
+        if (!(bg.classList.contains('on'))) {
+            bg.classList.add('on');
+        }
+    }
+})
+
+search_cls.addEventListener("click", (e)=>{
+    e.preventDefault();
+    main_header.classList.remove('search');
+    bg.classList.remove('on');
+})
+
+
+let scroll_section = document.querySelectorAll("[data-scroll]");
+let scroll_index = 0;
+
+let wheelTimer;
+
+window.addEventListener("wheel", function(e) {
+
+    e.preventDefault();
+
+    clearTimeout(wheelTimer);
+    wheelTimer = setTimeout(function() {
+  	    if(e.deltaY > 0) {
+            if(scroll_index == scroll_section.length) {
+                return;
+            }
+            else {
+                doScroll(++scroll_index);
+            }
+        }
+
+        if(e.deltaY < 0) {
+            if(scroll_index == 0) {
+                return;
+            }
+            else {
+                doScroll(--scroll_index);
+            }
+        }
+    },50    );
+}, {passive:false});
+
+function doScroll(scroll_index) {
+    scroll_index = scroll_index < 0 ? 0 : scroll_index;
+    scroll_index = scroll_index > scroll_section.length - 1 ? scroll_section.length - 1 : scroll_index;
+      
+    curSIdx = scroll_index;
+      
+    scroll_section[curSIdx].scrollIntoView({
+      block: "start", inline: "start", behavior: "smooth"
+    });  	
 }
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > scroll_section[1].offsetTop - 10) {
+        main_header.classList.add('white');
+        frame_idxbox.classList.add('on');
+    } else {
+        main_header.classList.remove('white');
+        frame_idxbox.classList.remove('on');
+    }
+})
