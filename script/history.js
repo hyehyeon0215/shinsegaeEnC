b9764a58ae057ff973e7fbd8747e9f26491f92b7
hyehyeon0215 <qwert0880@naver.com>
2023-08-24 18:10:00 +0900

    연혁 서브페이지 구현

--- script/history.js
+++ script/history.js
@@ -6,8 +6,6 @@ const sub_nav = document.querySelector(".sub-nav");
 const history_wrap = document.querySelector(".history-wrap");
 const history_content = document.querySelectorAll(".history-wrap > div");
 
-const progress = document.querySelector(".progress-bar2");
-
 
 
 window.addEventListener("scroll", ()=> {
@@ -15,24 +13,20 @@ window.addEventListener("scroll", ()=> {
     if(window.pageYOffset > history_nav.offsetTop - 100) {
         history_nav.classList.add("fixed");
         sub_nav.style.display = "none";
-
-        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        var scrolled = (winScroll / height) * 100;
-        progress.style.width = scrolled + "%";
-        var progress_width = progress.style.width;
-
-        if (progress_width == '100%') {
-            history_ul.classList.add("end");
-        }
-        else {
-            history_ul.classList.remove("end");
-        }
     }
     else {
         history_nav.classList.remove("fixed");
         sub_nav.style.display = "block";
+        history_li[0].classList.add("on");
     }
+
+    if(window.pageYOffset > history_content[history_content.length - 1].offsetTop + 1000) {
+        history_ul.classList.add("end");
+    }
+    else {
+        history_ul.classList.remove("end");
+    }
+
 
     history_content.forEach((el, index) => {
         var content_top = el.offsetTop;
@@ -42,18 +36,23 @@ window.addEventListener("scroll", ()=> {
         }
         else {
             history_li[index].classList.remove("on");
+            
         }
+
+        history_li[0].classList.add("on");
     })
 
-})
-
-history_li.forEach((el, index)=>{
-    el.addEventListener("click", (e)=>{
-        e.preventDefault();
-        history_li.forEach((el)=>{
-            el.classList.remove("on");
+    history_li.forEach((el, index)=>{
+        el.addEventListener("click", (e)=>{
+            e.preventDefault();
+    
+            history_li.forEach((el)=>{
+                el.classList.remove("on");
+            })
+            el.classList.add("on");
+            window.scrollTo({top: history_content[index].offsetTop + 200, behavior: "smooth"})
         })
-        el.classList.add("on");
-        window.scrollTo({top: history_content[index].offsetTop + 1, behavior: "smooth"})
     })
+
 })
+
